8b27d07978aa5b591ce03fb52cce3ea2bb43f948
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-06-18 23:14:06 +0200

    manual commit

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -20,7 +20,7 @@ wflow_publish(here::here("analysis",
                            "diagnostics_variables_XYZT.Rmd",
                            "diagnostics_variables_ancillary.Rmd"
                          )),
-              message = "rebuild all with March2021_v2 data version",
+              message = "drop non-existing files from overview",
               republish = TRUE)
 
 # Push latest version to GitHub
