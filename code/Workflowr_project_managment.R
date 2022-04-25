bbead76b9798332b56a8515bb14a5ac608088b1b
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-04-25 14:12:06 +0200

    manual commit

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -20,7 +20,7 @@ wflow_publish(here::here("analysis",
                            "diagnostics_variables_XYZT.Rmd",
                            "diagnostics_variables_ancillary.Rmd"
                          )),
-              message = "rerun with v20210707, the submitted files",
+              message = "rerun with 20220425",
               republish = TRUE)
 
 # Push latest version to GitHub
